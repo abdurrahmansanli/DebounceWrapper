39b8542545e3cd430b006474d8070ef2b316801b
Abdurrahman Şanlı <asanli@live.com>
2021-05-04 01:48:07 +0300

    Update Debouncer.swift
    Added option to cancel scheduled block.

--- Debouncer.swift
+++ Debouncer.swift
@@ -1,29 +1,33 @@
 class Debouncer {
-
+    
     private var blockToRun: (()->Void)?
-
+    
     private var timeIntervalDebounce: TimeInterval
-
+    
     private var timerRunBlockToRun: Timer?
-
+    
     init(timeIntervalDebounce: TimeInterval) {
         self.timeIntervalDebounce = timeIntervalDebounce
     }
-
+    
     func bounceWithBlock(blockToRun: (()->Void)?) {
         self.blockToRun = blockToRun
         cancelPreviouslyScheduledBlockAndSetScheduleForNewBlock()
     }
-
-    private func cancelPreviouslyScheduledBlockAndSetScheduleForNewBlock() {
+    
+    func cancelScheduledBlock() {
         timerRunBlockToRun?.invalidate()
+    }
+    
+    private func cancelPreviouslyScheduledBlockAndSetScheduleForNewBlock() {
+        cancelScheduledBlock()
         timerRunBlockToRun = Timer.scheduledTimer(timeInterval: timeIntervalDebounce,
                                                   target: self,
                                                   selector: #selector(runBlockToRun),
                                                   userInfo: nil,
                                                   repeats: false)
     }
-
+    
     @objc private func runBlockToRun() {
         blockToRun?()
     }
